befec04cf64f1aaa006eb323e989a1972c99487d
Henry <36207470+henryinqz@users.noreply.github.com>
2021-04-27 17:11:25 -0400

    Comment about tracking run.py for debugging

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+# Use `git update-index --assume-unchanged run.py` to ignore changes to this file (ex. entering debug mode)
+# (to track changes again, use `git update-index --no-assume-unchanged run.py `)
 from findyourev import app
 
 if __name__ == "__main__":
-    app.run(debug=False, threaded=True)
+    app.run(threaded=True, debug=False)
